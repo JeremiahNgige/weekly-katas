ee2f92b4df16148e9fdd70958b14cda040e9d8c1
Jeremiah Brian <brianjeremy123789@gmail.com>
2021-03-09 00:35:50 +0300

    matrix printing

---
+++ matrixs.py
@@ -0,0 +1,17 @@
+a = int(input('Enter no. of rows:'))
+b = int(input('Enter no. of columns:'))
+
+m = []
+
+for i in range(a):
+    c = []
+    for j in range(b):
+        j = int(input('Enter no. in Pocket ['+str(i)+'] ['+str(j)+'] '))
+        c.append(j)
+    m.append(c)
+print('Matrix is....')
+for i in range(a):
+    for j in range(b):
+        print(m[i][j], end=' ')
+        
+    print()
